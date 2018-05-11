4d1f8cf7a6e9e7677965da69a53b596fef0e1108
GMR516 <garrett@heywordboy.com>
2018-05-10 19:23:29 -0700

    Initial commit.

---
+++ FirefoxDarkNewTab/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Install dark theme into Firefox new tab? y/n");
+        char.TryParse(Console.ReadLine(), out char c);
+        if (c == 'y')
+        {
+            string roaming = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            Console.WriteLine(roaming);
+            Console.ReadKey();
+        }
+        else
+        {
+            Console.WriteLine("You declined. Please type 'y' (without the quotes) to install.");
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+            return;
+        }
+    }
+}
+
